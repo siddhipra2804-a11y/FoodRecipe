be694b69c270c4665ecaa6000f0319062fa8b84d
Prajapasti Siddhi <siddhipra2804@gmail.com>
2026-05-03 03:52:59 -0400

    feat: configure redux store with favorites reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import favoritesReducer from './favoritesSlice';
+import favoritesReducer from './favoritesSlice'; // Adjust the path if your slice file has a different name
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
+    // This maps the favoritesReducer to the 'favorites' state slice
     favorites: favoritesReducer,
   },
 });
 
+// Optional: If you need to use the store in your index.js/App.js
 export default store;
